cfa97c112d5a1d3b02094dcfe23c99091913d29e
sunglee42 <261275320+sunglee42@users.noreply.github.com>
2026-08-25 13:50:35 +0800

    Made some mark for Memory Modify

--- configs/licheea53nano/memmap.py
+++ configs/licheea53nano/memmap.py
@@ -40,10 +40,10 @@ class MemoryMap:
     # =================
     # Multimedia buffer. Used by u-boot/kernel/FreeRTOS
     # =================
-    ION_SIZE = 2 * SIZE_1M
+    ION_SIZE = 2 * SIZE_1M # modify from => 63 * SIZE_1M
     H26X_BITSTREAM_SIZE = 2 * SIZE_1M
     H26X_ENC_BUFF_SIZE = 0
-    ISP_MEM_BASE_SIZE = 2 * SIZE_1M
+    ISP_MEM_BASE_SIZE = 2 * SIZE_1M # modify from => 20 * SIZE_1M
     FREERTOS_RESERVED_ION_SIZE = H26X_BITSTREAM_SIZE + H26X_ENC_BUFF_SIZE + ISP_MEM_BASE_SIZE
 
     # ION after FreeRTOS
@@ -58,7 +58,7 @@ class MemoryMap:
 
     # Boot logo is after the ION buffer
     # Framebuffer uses boot logo's reserved memory
-    BOOTLOGO_SIZE = 2 * SIZE_1M
+    BOOTLOGO_SIZE = 2 * SIZE_1M # modify from => 5632 * SIZE_1K
     BOOTLOGO_ADDR = ION_ADDR - BOOTLOGO_SIZE
     FRAMEBUFFER_SIZE = BOOTLOGO_SIZE
     FRAMEBUFFER_ADDR = BOOTLOGO_ADDR
